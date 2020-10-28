12b45fbc179b5a0e4ecb171deea00db1aa7930b6
Opilane TTHK <opilane@tthk.ee>
2020-10-28 16:00:21 +0200

    dateinfo

---
+++ BirthdayOfWeek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BirthdayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What year were you born?");
+            int usersYear = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What number month were you born?");
+            int usersMonth = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What number day were you born?");
+            int usersDay = Int32.Parse(Console.ReadLine());
+
+            DateTime birthDate = new DateTime(usersYear, usersMonth, usersDay);
+            var dayOfTheWeek = birthDate.DayOfWeek;
+            Console.WriteLine($"You were born on {dayOfTheWeek}");
+        }
+    }
+}
---
+++ DateAndTime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+            Console.WriteLine($"Today: {today}");
+            int yearToday = today.Year;
+            Console.WriteLine($"Current year from today {yearToday}");
+            var weekDayToday = today.DayOfWeek;
+            Console.WriteLine($"It's currently {weekDayToday}");
+            Console.WriteLine("-------------------------");
+
+            DateTime todayDetails = DateTime.Now;
+            Console.WriteLine($"Today in details: {todayDetails}");
+            DateTime utc = DateTime.UtcNow;
+            Console.WriteLine($"The universal central time is {utc}");
+
+            //DateTime(year, month, day, hour, minute)
+
+            DateTime christmasEve = new DateTime(2020, 12, 24);
+            var christmasDayOfWeek = christmasEve.DayOfWeek;
+            Console.WriteLine($"Christmas eve is on {christmasDayOfWeek}");
+        }
+    }
+}
